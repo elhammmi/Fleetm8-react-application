dddfbe2abb41c6ddf7358b26b150eb6e29e99830
Elham Mirzavand <elham.mirzavand@gmail.com>
2024-01-23 17:24:41 +0100

    Add Tailwind and implement posts page

---
+++ src/components/Posts.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import Post from "./Post";
+
+const Posts = () => {
+  const [posts, setPost] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        if (!response.ok) {
+          return new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        setPost(result);
+        console.log(posts);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, [posts]);
+  return (
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-3xl font-bold mb-8">Posts</h1>
+      {posts.length > 0 &&
+        posts.map((post, index) => <Post key={index} {...post} />)}
+    </div>
+  );
+};
+
+export default Posts;
